7072e70f8479a5b9ee4aff777a3fce0f72d418ba
declanwalsh <deko.meister.dawg@gmail.com>
2016-02-26 10:00:07 +1100

    Added more comments to explain script

--- tunnelVelocityDTC.m
+++ tunnelVelocityDTC.m
@@ -1,10 +1,15 @@
-STATIC_PORT = 64;
-TOTAL_PORT = 1;
-DENSITY_AIR = 1.225;
+% short script that uses an already setup DTC object to measure the tunnel velocity
+% Author: Declan Walsh
+% Last Modified: 26/02/2016
 
-pOut = DTCMeasure(t, STBL);
+STATIC_PORT = 64; % port number the static pressure is connected to
+TOTAL_PORT = 1; % port number the total pressure is connected to
+DENSITY_AIR = 1.225; % density of air
 
-dynamicPDTC = pOut(TOTAL_PORT) - pOut(STATIC_PORT);
-velDTC = sqrt(2*dynamicPDTC/DENSITY_AIR);
+pOut = DTCMeasure(DTC, STBL); % measure the pressures on all ESP ports
 
+dynamicPDTC = pOut(TOTAL_PORT) - pOut(STATIC_PORT); % calculate the dynamic pressure
+velDTC = sqrt(2*dynamicPDTC/DENSITY_AIR); % calculate the velocity from dynamic pressure
+
+% print the resultant velocity to the user
 fprintf(1, 'The DTC tunnel velocity is %.2f m/s\n', velDTC);
